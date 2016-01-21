59bb130c1138b6af7caaa210000fe8d31d174c3b
Dhanshri Chirmade <dhanshri_chirmade@persistent.com>
2016-01-21 20:01:18 +0530

    20th jan commited

---
+++ 21-01-16/ExP/WebContent/source/js/index.js
@@ -0,0 +1,9 @@
+$('.toggle').on('click', function() {
+  $('.container1').stop().addClass('active');
+});
+
+$('.close').on('click', function() {
+  $('.container1').stop().removeClass('active');
+});
+
+
